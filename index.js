c3a98e240bce87a3531fb59cc3d295f71a3fda94
Aris Koumis <ariskoumis@gmail.com>
2018-04-12 15:38:02 -0700

    Print new task name when submit button is clicked

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function main() {
-	console.log("Hello World!");
+	document.getElementById("submit-task").addEventListener("click", submitTaskClicked);
+}
+
+function submitTaskClicked() {
+	var task_name = document.getElementById("task-name").value;
+	console.log("Task:" + task_name);
 }
 
 document.addEventListener('DOMContentLoaded', main);
